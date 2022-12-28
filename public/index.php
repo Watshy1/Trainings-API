8de0cf720d4ff5b8897d80218e94d51a09c59ac1
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-12-28 18:43:54 +0100

    add some dependencies and connect to DB

--- public/index.php
+++ public/index.php
@@ -4,22 +4,31 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
 
 $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, true, true);
 
-
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    $response->getBody()->write('Hello World!');
     return $response;
 });
 
-$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+$app->get('/trainings', function (Request $request, Response $response) {
+    $pdo = new PDO($_ENV['DB_TYPE'] . ':host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'] . ';user=' . $_ENV['DB_USER'] . ';password=' . $_ENV['DB_PASSWORD']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $statement = $pdo->prepare('SELECT * FROM test');
+    $statement->execute();
+
+    return $response->withJson($statement->fetchAll());
 });
 
 $app->run();
